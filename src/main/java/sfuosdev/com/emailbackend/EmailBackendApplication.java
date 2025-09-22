c3ae46b3042dda553a11d55848177495a41c1198
Tommy Oh <koa18@sfu.ca>
2025-09-21 17:19:14 -0700

    Initial commit

---
+++ src/main/java/sfuosdev/com/emailbackend/EmailBackendApplication.java
@@ -0,0 +1,13 @@
+package sfuosdev.com.emailbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sfuosdev/com/emailbackend/EmailBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package sfuosdev.com.emailbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
